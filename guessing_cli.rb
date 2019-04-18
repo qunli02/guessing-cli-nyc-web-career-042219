92f93596fe7ead986796a2fb62ee0cd0e2770029
Qun Li Huang <qunli02@gmail.com>
2019-04-18 18:07:32 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -2,7 +2,7 @@
 def run_guessing_game
   loop do
     anwser = 1 + rand(6)
-    puts "Guess a number bewtween 1 and 6."
+    puts "Guess a number between 1 and 6."
     input = gets.chomp
     if input == "exit"
       puts "Goodbye!"
