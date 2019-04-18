a4ab9c0c2558fa6084d9dfdc398190ecfdf18884
Qun Li Huang <qunli02@gmail.com>
2019-04-18 17:54:46 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,4 +1,14 @@
 # Code your solution here!
 def run_guessing_game
- anwser = return 1 + rand(6)
+  anwser = return 1 + rand(6)
+  input = gets.chomp
+  if input == "exit"
+    puts "Goodbye"
+    break
+  elsif input == anwser
+    puts "You guessed the correct number!"
+    break
+  else
+    puts "The computer guessed 5."
+  end
 end
